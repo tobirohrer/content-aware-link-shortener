952f8faae945dce8f441a5b36257a4a949641a18
Tobias Rohrer <tobias.rohrer@outlook.com>
2018-11-30 19:39:25 +0100

    v0.0001

---
+++ backend/webservice.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request, redirect
+import requests
+import json
+from random import randint
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+link_elements = ["cat", "dog", "blue", "nerd", "fun", "today", "kuchen", "hopfen", "bier", "code", "ball", "klettern", "skate", "shit"]
+links = {}
+
+@app.route('/<funny_link>', methods=["GET"])
+def do_redirect(funny_link):
+    redirect_to = links[funny_link]
+    return redirect(redirect_to, code=302)
+
+@app.route('/links', methods=["POST"])
+def post_user():
+    target = request.get_json()
+    url = get_random_url()
+    links[url] = target['target']
+    return jsonify(links), 200
+
+def get_random_url():
+    url = ""
+    num_of_words = randint(2,5)
+
+    for x in range (0, num_of_words):
+        if x != 0:
+            url = url + "-" + link_elements[randint(0, len(link_elements)-1)]
+        if x == 0:
+            url = url + link_elements[randint(0, len(link_elements)-1)]
+    return url
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
