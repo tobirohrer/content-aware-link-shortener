a93699d264d4f64852b733b193d00703ee894e5d
Tobias Rohrer <tobias.rohrer@outlook.com>
2018-12-07 09:01:36 +0100

    changed db path

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 import pickledb 
 
-link_db = pickledb.load('links.db', False)
-stats_db = pickledb.load('stats.db', False)
+link_db = pickledb.load('./store/links.db', False)
+stats_db = pickledb.load('./store/stats.db', False)
 
 def get_target(url):
     return link_db.get(url)
