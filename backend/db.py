90343f159081943bb911df78074ad513eb7c91cd
Your Name <tobias.rohrer@outlook.com>
2019-08-25 23:27:38 +0200

    loading pickledb with every endpoint call

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,15 @@
 import pickledb 
 
-link_db = pickledb.load('/analyse/links.db', False)
-stats_db = pickledb.load('/analyse/stats.db', False)
 #link_db = pickledb.load('./links.db', False)
 #stats_db = pickledb.load('./stats.db', False)
 
 def get_target(url):
+    link_db = pickledb.load('/analyse/links.db', False)
     return link_db.get(url)
 
 def set_target(url, target):
+    link_db = pickledb.load('/analyse/links.db', False)
+    stats_db = pickledb.load('/analyse/stats.db', False)
     link_db.set(url, target)
     stats_db.set(url, 0)
     link_db.dump()
@@ -16,11 +17,14 @@ def set_target(url, target):
     return link_count
 
 def update_url_stats(url):
+    stats_db = pickledb.load('/analyse/stats.db', False)
     stats_db.set(url, stats_db.get(url)+1)
     stats_db.dump()
 
 def get_url_stats(url):
+    stats_db = pickledb.load('/analyse/stats.db', False)
     return stats_db.get(url)
 
 def get_link_count():
+    link_db = pickledb.load('/analyse/links.db', False)
     return len(link_db.getall())
